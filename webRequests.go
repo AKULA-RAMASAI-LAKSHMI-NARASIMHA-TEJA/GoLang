789b5ac40415f567d996e493f20efde954786ee0
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <21A91A6139@aec.edu.in>
2025-06-03 23:31:38 +0530

    web requests

---
+++ webRequests.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://example.com"
+
+func main() {
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%T\n", response) // *http.Response
+
+	data, err := ioutil.ReadAll(response.Body) // returns data in bytes
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
+
+	defer response.Body.Close() // best practice
+}
